dc791f8d9e4100c38c0162a1aa737a510324a01e
vitquay <vitquay1996@gmail.com>
2016-06-10 10:46:30 +0700

    fix graph

--- admin.php
+++ admin.php
@@ -1,6 +1,3 @@
-<?php error_reporting(E_ALL);
-ini_set('display_errors', 1);
-?>
 <?php function dateformat($date1){
   $date = new DateTime($date1);
   $day = $date->format("d");
@@ -89,28 +86,28 @@ include_once 'models/DateCount.php';
             </div>
 
 
-            </div>
-            <!-- /sidebar menu -->
-
-            <!-- /menu footer buttons -->
-            <div class="sidebar-footer hidden-small">
-              <a href="index.php" data-toggle="tooltip" data-placement="top" title="Lock">
-                <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
-              </a>
-              <a href="logout.php" data-toggle="tooltip" data-placement="top" title="Logout">
-                <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
-              </a>
-            </div>
-            <!-- /menu footer buttons -->
           </div>
-        </div>
+          <!-- /sidebar menu -->
 
-        <!-- page content -->
-        <div class="right_col" role="main">
-          <!-- top tiles -->
-          <div class="row tile_count">
-            <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
-              <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
+          <!-- /menu footer buttons -->
+          <div class="sidebar-footer hidden-small">
+            <a href="index.php" data-toggle="tooltip" data-placement="top" title="Lock">
+              <span class="glyphicon glyphicon-home" aria-hidden="true"></span>
+            </a>
+            <a href="logout.php" data-toggle="tooltip" data-placement="top" title="Logout">
+              <span class="glyphicon glyphicon-off" aria-hidden="true"></span>
+            </a>
+          </div>
+          <!-- /menu footer buttons -->
+        </div>
+      </div>
+
+      <!-- page content -->
+      <div class="right_col" role="main">
+        <!-- top tiles -->
+        <div class="row tile_count">
+          <div class="col-md-3 col-sm-4 col-xs-6 tile_stats_count">
+            <span class="count_top"><i class="fa fa-user"></i> Total Users</span>
         <?php //find total number of users
         $Count = new Count();
         $numberUser = $Count->totalUser();
@@ -197,10 +194,6 @@ include_once 'models/DateCount.php';
             <h3>Posts Activities</h3>
           </div>
           <div class="col-md-6">
-            <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
-              <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
-              <span>December 30, 2014 - January 28, 2015</span> <b class="caret"></b>
-            </div>
           </div>
         </div>
 
@@ -218,7 +211,7 @@ include_once 'models/DateCount.php';
 
   </div>
   <br />
-
+</body>
 
 
   
@@ -232,8 +225,7 @@ include_once 'models/DateCount.php';
     <div class="clearfix"></div>
   </footer>
   <!-- /footer content -->
-</div>
-</div>
+
 
 <!-- jQuery -->
 <script src="vendors/jquery/dist/jquery.min.js"></script>
@@ -245,14 +237,9 @@ include_once 'models/DateCount.php';
 <script src="vendors/nprogress/nprogress.js"></script>
 <!-- Chart.js -->
 <script src="vendors/Chart.js/dist/Chart.min.js"></script>
-<!-- gauge.js -->
-<script src="vendors/bernii/gauge.js/dist/gauge.min.js"></script>
-<!-- bootstrap-progressbar -->
-<script src="vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
-<!-- iCheck -->
-<script src="vendors/iCheck/icheck.min.js"></script>
-<!-- Skycons -->
-<script src="vendors/skycons/skycons.js"></script>
+
+
+
 <!-- Flot -->
 <script src="vendors/Flot/jquery.flot.js"></script>
 <script src="vendors/Flot/jquery.flot.pie.js"></script>
@@ -264,8 +251,7 @@ include_once 'models/DateCount.php';
 <script src="js/flot/date.js"></script>
 <script src="js/flot/jquery.flot.spline.js"></script>
 <script src="js/flot/curvedLines.js"></script>
-<!-- jVectorMap -->
-<script src="js/maps/jquery-jvectormap-2.0.3.min.js"></script>
+
 <!-- bootstrap-daterangepicker -->
 <script src="js/moment/moment.min.js"></script>
 <script src="js/datepicker/daterangepicker.js"></script>
@@ -289,48 +275,40 @@ $(document).ready(function() {
     [gd('.$date[2].','.$date[1].','.$date[0].'), '.$data->count.'],';
   }?>];
 
-  var data2 = [
-                // [gd(2012, 1, 1), 21],
-                // // [gd(2012, 1, 2), 12],
-                // // [gd(2012, 1, 3), 20],
-                // // [gd(2012, 1, 4), 20],
-                // // [gd(2012, 1, 5), 11],
-                // // [gd(2012, 1, 6), 20],
-                // [gd(2012, 1, 7), 20]
-                ];
-                $("#canvas_dahs").length && $.plot($("#canvas_dahs"), [
-                  data1, data2
-                  ], {
-                    series: {
-                      lines: {
-                        show: false,
-                        fill: true
-                      },
-                      splines: {
-                        show: true,
-                        tension: 0.4,
-                        lineWidth: 1,
-                        fill: 0.4
-                      },
-                      points: {
-                        radius: 0,
-                        show: true
-                      },
-                      shadowSize: 2
-                    },
-                    grid: {
-                      verticalLines: true,
-                      hoverable: true,
-                      clickable: true,
-                      tickColor: "#d5d5d5",
-                      borderWidth: 1,
-                      color: '#fff'
-                    },
-                    colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
-                    xaxis: {
-                      tickColor: "rgba(51, 51, 51, 0.06)",
-                      mode: "time",
-                      tickSize: [1, "day"],
+
+  $("#canvas_dahs").length && $.plot($("#canvas_dahs"), [
+    data1
+    ], {
+      series: {
+        lines: {
+          show: false,
+          fill: true
+        },
+        splines: {
+          show: true,
+          tension: 0.4,
+          lineWidth: 1,
+          fill: 0.4
+        },
+        points: {
+          radius: 0,
+          show: true
+        },
+        shadowSize: 2
+      },
+      grid: {
+        verticalLines: true,
+        hoverable: true,
+        clickable: true,
+        tickColor: "#d5d5d5",
+        borderWidth: 1,
+        color: '#fff'
+      },
+      colors: ["rgba(38, 185, 154, 0.38)", "rgba(3, 88, 106, 0.38)"],
+      xaxis: {
+        tickColor: "rgba(51, 51, 51, 0.06)",
+        mode: "time",
+        tickSize: [1, "day"],
                       //tickLength: 10,
                       axisLabel: "Date",
                       axisLabelUseCanvas: true,
@@ -351,195 +329,5 @@ function gd(year, month, day) {
 });
 </script>
 <!-- /Flot -->
-
-<!-- jVectorMap -->
-<script src="js/maps/jquery-jvectormap-world-mill-en.js"></script>
-<script src="js/maps/jquery-jvectormap-us-aea-en.js"></script>
-<script src="js/maps/gdp-data.js"></script>
-<script>
-$(document).ready(function(){
-  $('#world-map-gdp').vectorMap({
-    map: 'world_mill_en',
-    backgroundColor: 'transparent',
-    zoomOnScroll: false,
-    series: {
-      regions: [{
-        values: gdpData,
-        scale: ['#E6F2F0', '#149B7E'],
-        normalizeFunction: 'polynomial'
-      }]
-    },
-    onRegionTipShow: function(e, el, code) {
-      el.html(el.html() + ' (GDP - ' + gdpData[code] + ')');
-    }
-  });
-});
-</script>
-<!-- /jVectorMap -->
-
-<!-- Skycons -->
-<script>
-$(document).ready(function() {
-  var icons = new Skycons({
-    "color": "#73879C"
-  }),
-  list = [
-  "clear-day", "clear-night", "partly-cloudy-day",
-  "partly-cloudy-night", "cloudy", "rain", "sleet", "snow", "wind",
-  "fog"
-  ],
-  i;
-
-  for (i = list.length; i--;)
-    icons.set(list[i], list[i]);
-
-  icons.play();
-});
-</script>
-<!-- /Skycons -->
-
-<!-- Doughnut Chart -->
-<script>
-$(document).ready(function(){
-  var options = {
-    legend: false,
-    responsive: false
-  };
-
-  new Chart(document.getElementById("canvas1"), {
-    type: 'doughnut',
-    tooltipFillColor: "rgba(51, 51, 51, 0.55)",
-    data: {
-      labels: [
-      "Symbian",
-      "Blackberry",
-      "Other",
-      "Android",
-      "IOS"
-      ],
-      datasets: [{
-        data: [15, 20, 30, 10, 30],
-        backgroundColor: [
-        "#BDC3C7",
-        "#9B59B6",
-        "#E74C3C",
-        "#26B99A",
-        "#3498DB"
-        ],
-        hoverBackgroundColor: [
-        "#CFD4D8",
-        "#B370CF",
-        "#E95E4F",
-        "#36CAAB",
-        "#49A9EA"
-        ]
-      }]
-    },
-    options: options
-  });
-});
-</script>
-<!-- /Doughnut Chart -->
-
-<!-- bootstrap-daterangepicker -->
-<script>
-$(document).ready(function() {
-
-  var cb = function(start, end, label) {
-    console.log(start.toISOString(), end.toISOString(), label);
-    $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
-  };
-
-  var optionSet1 = {
-    startDate: moment().subtract(29, 'days'),
-    endDate: moment(),
-    minDate: '01/01/2012',
-    maxDate: '12/31/2015',
-    dateLimit: {
-      days: 60
-    },
-    showDropdowns: true,
-    showWeekNumbers: true,
-    timePicker: false,
-    timePickerIncrement: 1,
-    timePicker12Hour: true,
-    ranges: {
-      'Today': [moment(), moment()],
-      'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-      'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-      'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-      'This Month': [moment().startOf('month'), moment().endOf('month')],
-      'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
-    },
-    opens: 'left',
-    buttonClasses: ['btn btn-default'],
-    applyClass: 'btn-small btn-primary',
-    cancelClass: 'btn-small',
-    format: 'MM/DD/YYYY',
-    separator: ' to ',
-    locale: {
-      applyLabel: 'Submit',
-      cancelLabel: 'Clear',
-      fromLabel: 'From',
-      toLabel: 'To',
-      customRangeLabel: 'Custom',
-      daysOfWeek: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
-      monthNames: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
-      firstDay: 1
-    }
-  };
-  $('#reportrange span').html(moment().subtract(29, 'days').format('MMMM D, YYYY') + ' - ' + moment().format('MMMM D, YYYY'));
-  $('#reportrange').daterangepicker(optionSet1, cb);
-  $('#reportrange').on('show.daterangepicker', function() {
-    console.log("show event fired");
-  });
-  $('#reportrange').on('hide.daterangepicker', function() {
-    console.log("hide event fired");
-  });
-  $('#reportrange').on('apply.daterangepicker', function(ev, picker) {
-    console.log("apply event fired, start/end dates are " + picker.startDate.format('MMMM D, YYYY') + " to " + picker.endDate.format('MMMM D, YYYY'));
-  });
-  $('#reportrange').on('cancel.daterangepicker', function(ev, picker) {
-    console.log("cancel event fired");
-  });
-  $('#options1').click(function() {
-    $('#reportrange').data('daterangepicker').setOptions(optionSet1, cb);
-  });
-  $('#options2').click(function() {
-    $('#reportrange').data('daterangepicker').setOptions(optionSet2, cb);
-  });
-  $('#destroy').click(function() {
-    $('#reportrange').data('daterangepicker').remove();
-  });
-});
-</script>
-<!-- /bootstrap-daterangepicker -->
-
-<!-- gauge.js -->
-<script>
-var opts = {
-  lines: 12,
-  angle: 0,
-  lineWidth: 0.4,
-  pointer: {
-    length: 0.75,
-    strokeWidth: 0.042,
-    color: '#1D212A'
-  },
-  limitMax: 'false',
-  colorStart: '#1ABC9C',
-  colorStop: '#1ABC9C',
-  strokeColor: '#F0F3F3',
-  generateGradient: true
-};
-var target = document.getElementById('foo'),
-gauge = new Gauge(target).setOptions(opts);
-
-gauge.maxValue = 6000;
-gauge.animationSpeed = 32;
-gauge.set(3200);
-gauge.setTextField(document.getElementById("gauge-text"));
-</script>
-<!-- /gauge.js -->
 </body>
 </html>
